8468590d3ea29299d2460be677696b528bad12f3
Harsh <harxhist@gmail.com>
2026-03-31 16:01:10 +0530

    Add shortcut, update images
    Signed-off-by: Harsh <harxhist@gmail.com>

--- fe/src/pages/shorten.js
+++ fe/src/pages/shorten.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import Footer from '../components/Footer';
+import Header from '../components/Header';
 import ios from '../../public/shorten.png';
 import shortcut from '../../public/shortcut.png';
-import Header from '../components/Header';
 import one from '../../public/1.png';
 import two from '../../public/2.png';
 import three from '../../public/3.png';
-import four from '../../public/4.png';
 
 const Shorten = () => {
   const steps = [
-    { image: one, title: 'Normal Browsing ', description: 'The user visits any blog or webpage & needs TL;DR for it' },
+    { image: one, title: 'Normal Browsing', description: 'The user visits any blog or webpage & needs TL;DR for it' },
     { image: two, title: 'Adding the Prefix', description: 'Before the original URL, insert "sh10.in/".' },
     { image: three, title: 'Getting Gist', description: 'Summary will appear. Click "Listen" to hear it.' },
   ];
@@ -21,51 +21,56 @@ const Shorten = () => {
       <Header />
       <div className="bg-[#282828] py-0 px-4 md:px-6">
         <div className="max-w-screen-lg mx-auto flex flex-col md:flex-row items-center md:space-x-12">
+          
           <div className="w-1/2 hidden md:block">
-            <Image src={ios} alt="shorten" width={500} height={500} />
+            <Image src={ios} alt="shorten preview" width={500} height={500} />
           </div>
+          
           <div className="pb-8 text-center md:text-left">
-            <a href="/">
-            </a>
             <h1 className="mt-12 text-4xl text-white font-thin">Summarise w/ shorten</h1>
             <p className="mt-4 text-base text-white font-light">Get a gist and listen to it.</p>
-            <a href="/ios-shortcut" target="_blank">
-              <div className="mt-12 inline-flex items-center py-1.5 px-4 rounded-xl space-x-2 text-white hover:shadow-lg bg-[#1D1F57]">
-                <Image src={shortcut} width={60} height={60} />
-                <div className="px-2">
-                  <p className="font-light leading-none">Download the</p>
-                  <h3 className="text-3xl font-semibold leading-none">
-                    Shortcut
-                  </h3>
-                </div>
+            
+            {/* Fixed Link Component */}
+            <Link 
+              href="https://www.icloud.com/shortcuts/f7534d2395214947af199a878ded7acc" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="mt-12 inline-flex items-center py-1.5 px-4 rounded-xl space-x-2 text-white hover:shadow-lg bg-[#1D1F57]"
+            >
+              <Image src={shortcut} alt="iOS Shortcut Icon" width={60} height={60} />
+              <div className="px-2">
+                <p className="font-light leading-none">Download the</p>
+                <h3 className="text-3xl font-semibold leading-none">Shortcut</h3>
               </div>
-            </a>
+            </Link>
           </div>
-          <div className="md:hidden text-white px-12">
-            <Image src={ios} alt="shorten" width={500} height={500} />
+          
+          <div className="md:hidden text-white px-12 mt-8 md:mt-0">
+            <Image src={ios} alt="shorten preview mobile" width={500} height={500} />
           </div>
         </div>
       </div>
+
       <div className="max-w-screen-lg mx-auto px-4 md:px-6 py-8">
         <div className="py-20 px-4 md:px-6 flex flex-col md:flex-row gap-12 items-center justify-between">
-
           <div className="flex flex-col space-y-4">
             <h2 className="text-3xl text-white font-thin">Simplify Jargon</h2>
-            <ul className="text-white list-none space-y-2">
+            <ul className="text-white list-disc pl-5 space-y-2">
               <li>Summarizes most webpages.</li>
               <li>Listen to summarized webpage.</li>
             </ul>
           </div>
           <div className="flex flex-col space-y-4">
-            <h2 className="text-3xl text-white font-thin"> Simple Ways</h2>
-            <ul className="text-white list-none space-y-2">
+            <h2 className="text-3xl text-white font-thin">Simple Ways</h2>
+            <ul className="text-white list-disc pl-5 space-y-2">
               <li>Prefix any URL with "sh10.in/"</li>
-              <li>Use <em>Shorten</em> shortcut on Mac/ios.</li>
+              <li>Use <em>Shorten</em> shortcut on Mac/iOS.</li>
             </ul>
           </div>
         </div>
+
         <div className="prose-lg text-white gap-6">
-          <h2 className='md:px-6 px-4 text-3xl text-white font-normal text-center mt-10'>Guide</h2>
+          <h2 className="md:px-6 px-4 text-3xl text-white font-normal text-center mt-10">Guide</h2>
           <br />
           <br />
           <div className="space-y-12">
@@ -82,15 +87,9 @@ const Shorten = () => {
                     />
                   </div>
                 </div>
-                <div className="md:w-1/2 p-6">
-                  <div className="md:block hidden">
-                    <h3 className='text-[#f2bb4b] text-lg'>{step.title}</h3>
-                    <p>{step.description}</p>
-                  </div>
-                  <div className="md:hidden text-center">
-                    <h3 className='text-[#f2bb4b] text-lg'>{step.title}</h3>
-                    <p>{step.description}</p>
-                  </div>
+                <div className="md:w-1/2 p-6 text-center md:text-left">
+                  <h3 className="text-[#f2bb4b] text-lg">{step.title}</h3>
+                  <p>{step.description}</p>
                 </div>
               </div>
             ))}
